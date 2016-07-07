081fbbf4dc617d402f4f97a048f2be895fefd4ba
Alex Efremov <alex.efremov.dev@gmail.com>
2016-07-07 21:50:35 +0300

    Add App component

--- learning-react/scripts/main.js
+++ learning-react/scripts/main.js
@@ -1,17 +1,75 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+/*
+  App
+*/
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+
+});
+
+/*
+  Header
+*/
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/*
+  Order
+*/
+
+var Order = React.createClass({
+  render: function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+  Inventory
+*/
+
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/*
+  StorePicker
+*/
 var StorePicker = React.createClass({
 
   render: function() {
     return (
       <form className="store-selector">
         <h2>Please enter a store:</h2>
-        <input type="text" ref="storeId" required/>
+        <input type="text" ref="storeId" required />
         <input type="Submit" />
       </form>
     )
   }
 });
 
-ReactDOM.render(<StorePicker />, document.getElementById('main'));
+ReactDOM.render(<App />, document.getElementById('main'));
