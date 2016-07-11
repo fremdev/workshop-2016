0a4812854cd4dd2b0e8b9afa7989756f6ff6995e
Alex Efremov <alex.efremov.dev@gmail.com>
2016-07-11 14:40:54 +0300

    Add removeFromOrder method

--- learning-react/scripts/main.js
+++ learning-react/scripts/main.js
@@ -44,6 +44,10 @@ var App = React.createClass({
     this.state.order[key] = this.state.order[key] + 1 || 1;
     this.setState({order: this.state.order});
   },
+  removeFromOrder: function(key) {
+    delete this.state.order[key];
+    this.setState({order: this.state.order});
+  },
   addFish: function(fish) {
     var timestamp = (new Date()).getTime();
     // update the state object
@@ -74,7 +78,7 @@ var App = React.createClass({
             {Object.keys(this.state.fishes).map(this.renderFish)}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish} />
       </div>
     )
@@ -175,9 +179,10 @@ var Order = React.createClass({
   renderOrder: function(key) {
     var fish = this.props.fishes[key];
     var count = this.props.order[key];
+    var removeButton = <button onClick={this.props.removeFromOrder.bind(null,key)}>&times;</button>
 
     if(!fish) {
-      return <li key={key}>Sorry, fish no longer available!</li>
+      return <li key={key}>Sorry, fish no longer available! {removeButton}</li>
     }
 
     return (
@@ -185,6 +190,7 @@ var Order = React.createClass({
         {count}lbs
         {fish.name}
         <span className="price">{h.formatPrice(count * fish.price)}</span>
+        {removeButton}
       </li>
     )
   },
