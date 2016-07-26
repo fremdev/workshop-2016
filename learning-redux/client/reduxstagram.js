f3c8e969d0fcea721a6ded23e5e297cd1e87f804
Alex Efremov <alex.efremov.dev@gmail.com>
2016-07-26 23:02:36 +0300

    Add basic structure for components(Main, Photogrid, SIngle)

--- learning-redux/client/reduxstagram.js
+++ learning-redux/client/reduxstagram.js
@@ -1,1 +1,24 @@
-// let's go!
+import React from 'react';
+
+import {render} from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+// Import components
+import Main from './components/Main';
+import Single from './components/Single';
+import Photogrid from './components/Photogrid';
+
+// Import react router deps
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={Photogrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+render(router, document.getElementById('root'));
