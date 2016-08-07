b4c870c52ab48a5a9a592597a19590cba965e4ff
Alex Efremov <alex.efremov.dev@gmail.com>
2016-08-07 14:05:19 +0300

    Add simple animation using timeline

--- learning-gsap/app/scripts/timeline.js
+++ learning-gsap/app/scripts/timeline.js
@@ -1,9 +1,17 @@
 import { TweenLite, CSSPlugin, TimelineLite, EasePack } from 'gsap';
 
 function initTimeline() {
-  const toolTimeline = new TimelineLite();
-  toolTimeline.add(TweenLite.to('#toolBox', 1, {y:-80}));
-  toolTimeline.add(TweenLite.to('#weightLifter', 1, {y:-80}));
+  const toolTimeline = new TimelineLite({paused: true});
+  const duration = 1;
+  toolTimeline.add(TweenLite.to('#toolBox', duration, {y:-80, ease: Linear.easeInOut}));
+  toolTimeline.add(TweenLite.to('#weightLifter', duration, {y:-80, ease: Bounce.easeInOut}));
+  toolTimeline.add(TweenLite.to('#crazy', duration, {y:-80, ease: Elastic.easeInOut}));
+  toolTimeline.add(TweenLite.to('#leaf', duration, {y:-80, ease: Back.easeInOut}));
+
+  document.getElementById('start').addEventListener('click', () => {toolTimeline.play()});
+  document.getElementById('pause').addEventListener('click', () => {toolTimeline.pause()});
+  document.getElementById('stop').addEventListener('click', () => {toolTimeline.stop()});
+  document.getElementById('reverse').addEventListener('click', () => {toolTimeline.reverse()});
 }
 
 export default initTimeline;
