6caa47a2a45827c70611adf1afd63b9ebb6cd36b
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-01 20:57:52 +0200

    Add comditional and loop vue.js examples

---
+++ vuejs-examples/playground/ex10/app.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#app',
+  data: {
+    show: true,
+    ingredients: ['meat', 'fruits', 'cookies'],
+    persons: [
+      {name: 'Max', age: 27, color: 'red'},
+      {name: 'Anna', age: 'unknown', color: 'blue'}
+    ]
+  }
+});
---
+++ vuejs-examples/playground/ex11/app.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#app',
+  data: {
+    showFirst: true,
+    languages: ['JavaScript', 'Python', 'Ruby', 'C++', 'Java', 'Basic', 'Pascal', 'PHP', 'C#', 'C', 'Swift', 'Go', 'Haskell', 'Scala'],
+    book: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: 3
+    },
+    testObj: {
+      name: 'TEST_OBJECT',
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});
