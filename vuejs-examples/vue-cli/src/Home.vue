5be7eaad8fe3993d2f14851646e103fd65152d76
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-02 23:54:33 +0200

    Add ServerStatus component, using components globally/locally

--- vuejs-examples/vue-cli/src/Home.vue
+++ vuejs-examples/vue-cli/src/Home.vue
@@ -1,23 +1,14 @@
 <template>
   <div>
-    <p>Server Status: {{ status }}</p>
-    <hr>
-    <button @click="changeStatus">Change Status</button>
-  </div>
-
+    <app-server-status v-for="server in 5"></app-server-status>
+  </div>  
 </template>
 
 <script>
+  import ServerStatus from './ServerStatus.vue';
   export default {
-    data: function() {
-      return {
-        status: 'Critical'
-      }
-    },
-    methods: {
-      changeStatus() {
-        this.status = (this.status === 'Critical')? 'Normal' : 'Critical';
-      }
+    components: {
+      'app-server-status': ServerStatus
     }
   }
 </script>
