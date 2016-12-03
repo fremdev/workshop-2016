9171a595c653cd633c7300e2898c9beb14137428
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-03 20:11:42 +0200

    Add prop validation, child-parent communication(emit custom event)

--- vuejs-examples/playground/ex14/src/components/UserDetail.vue
+++ vuejs-examples/playground/ex14/src/components/UserDetail.vue
@@ -3,12 +3,26 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ name }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
   export default {
-    props: ['name']
+    props: {
+      name: {
+        type: String,
+        required: true
+        // It's possible to use default value:
+        // default: 'Alex'
+      }
+    },
+    methods: {
+      resetName() {
+        this.name = 'Alex';
+        this.$emit('nameWasReset', this.name);
+      }
+    }
   }
 </script>
 
