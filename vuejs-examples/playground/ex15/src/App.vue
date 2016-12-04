6458df68743e4cf204f0bb1f12716460c22baf30
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-04 17:03:35 +0200

    Use dynamic components, keep-alive

--- vuejs-examples/playground/ex15/src/App.vue
+++ vuejs-examples/playground/ex15/src/App.vue
@@ -2,11 +2,22 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-              <app-quote>
+              <button @click="selectedComponent = 'appQuote'">Quote</button>
+              <button @click="selectedComponent = 'appAuthor'">Author</button>
+              <button @click="selectedComponent = 'appNew'">New</button>
+              <hr>
+              <p>{{ selectedComponent }}</p>
+              <keep-alive>
+                <component :is="selectedComponent">
+                  <p>Default content</p>
+                </component>
+              </keep-alive>
+
+              <!-- <app-quote>
                 <h2 slot="title">Quote</h2>
-                <p slot="content">Wonderful Quote!</p>
+                <p slot="content">Wonderful Quote!</p> -->
                 <!-- It's possible to use named slot + default slot without name -->
-              </app-quote>
+              <!-- </app-quote> -->
             </div>
         </div>
     </div>
@@ -14,10 +25,19 @@
 
 <script>
   import Quote from './components/Quote.vue';
+  import Author from './components/Author.vue';
+  import New from './components/New.vue';
 
   export default {
     components: {
-      appQuote: Quote
+      appQuote: Quote,
+      appAuthor: Author,
+      appNew: New
+    },
+    data: function() {
+      return {
+        selectedComponent: 'appQuote'
+      };
     }
   }
 </script>
---
+++ vuejs-examples/playground/ex15/src/components/New.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <p>New</p>
+</template>
+
+<script>
+export default {
+  deactivated() {
+    console.log('was deactivated');
+  },
+  activated() {
+    console.log('was activated');
+  }
+}
+</script>
+
+<style lang="css">
+</style>
