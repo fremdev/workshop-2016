4b463640889c5ab58aa0c64c26280001652e4dde
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-04 12:33:24 +0200

    Add ex15 - using slots

---
+++ vuejs-examples/playground/ex15/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <app-quote>
+                <h2 slot="title">Quote</h2>
+                <p slot="content">Wonderful Quote!</p>
+                <!-- It's possible to use named slot + default slot without name -->
+              </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+  export default {
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuejs-examples/playground/ex15/src/components/Quote.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div>
+    <div class="title">
+      <slot name="title"></slot>
+      <!-- It's possible to use default content for slot if content is not provided -->
+      <span><slot name="subtitle">The Subtitle</slot></span>
+    </div>
+    <hr>
+    <div>
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+  h2 {
+    color: #5585c2;
+  }
+  .title {
+    font-style: italic;
+  }
+</style>
