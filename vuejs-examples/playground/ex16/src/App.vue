4bcc028c8de6cd20c3b2e7021d5cda653964e4e1
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-04 17:43:21 +0200

    Add ex16 - using slots and dynamic components example

---
+++ vuejs-examples/playground/ex16/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selectedComponent='appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent='appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent='appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <keep-alive>
+                  <component :is="selectedComponent">
+                    <h2>Vue.js and Alex present: Switch Color</h2>
+                    <p>Vue.js is awesome! You should try it!</p>
+                  </component>
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data: () => ({
+          selectedComponent: 'appBlue'
+        })
+    }
+</script>
+
+<style>
+</style>
