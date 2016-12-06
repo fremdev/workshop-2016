86dbf17dcef7fd144e924878a9a21a714d071b34
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-06 21:20:35 +0200

    Add more css/js animation examples to ex22

--- vuejs-examples/playground/ex22-animations/src/App.vue
+++ vuejs-examples/playground/ex22-animations/src/App.vue
@@ -16,9 +16,42 @@
                     This is some Info
                   </div>
                 </transition>
-                <transition name="fade">
+                <transition
+                  appear
+                  appearActiveClass="animated bounce"
+                  enter-active-class="animated bounce"
+                  leave-active-class="animated shake"
+                >
                   <div class="alert alert-info" v-if="show">
                     This is some Info
+                  </div>
+                </transition>
+                <br>
+                <hr>
+                <transition name="fade" mode="out-in">
+                  <div class="alert alert-info" v-if="show" key="info">
+                    This is some Info
+                  </div>
+                  <div class="alert alert-warning" v-else key="warning">
+                    This is some Warning
+                  </div>
+                </transition>
+                <hr>
+                <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+                <br><br>
+                <transition
+                  @before-enter="beforeEnter"
+                  @enter="enter"
+                  @after-enter="afterEnter"
+                  @enter-cancelled="enterCancelled"
+
+                  @before-leave="beforeLeave"
+                  @leave="leave"
+                  @after-leave="afterLeave"
+                  @leave-cancelled="leaveCancelled"
+                  :css="false"
+                  >
+                  <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load">
                   </div>
                 </transition>
             </div>
@@ -30,8 +63,58 @@
     export default {
         data() {
             return {
-              show: false
+              show: false,
+              load: false,
+              elementWidth: 100
             }
+        },
+        methods: {
+          beforeEnter(el) {
+            console.log('beforeEnter');
+            this.elementWidth = 100;
+            el.style.width = this.elementWidth + 'px';
+          },
+          enter(el, done) {
+            console.log('enter');
+            let round = 1;
+            const interval = setInterval(() => {
+              el.style.width = (this.elementWidth + round * 10) + 'px';
+              round++;
+              if (round > 20) {
+                clearInterval(interval);
+                done();
+              }
+            }, 20);
+          },
+          afterEnter(el) {
+            console.log('afterEnter');
+          },
+          enterCancelled(el) {
+            console.log('enterCancelled');
+          },
+          beforeLeave(el) {
+            console.log('beforeLeave');
+            this.elementWidth = 300;
+            el.style.width = this.elementWidth + 'px';
+          },
+          leave(el, done) {
+            console.log('leave');
+            let round = 1;
+            const interval = setInterval(() => {
+              el.style.width = (this.elementWidth - round * 10) + 'px';
+              round++;
+              if (round > 20) {
+                clearInterval(interval);
+                done();
+              }
+            }, 20);
+          },
+          afterLeave(el) {
+            console.log('afterLeave');
+          },
+          leaveCancelled(el) {
+            console.log('leaveCancelled');
+          }
         }
     }
 </script>
@@ -56,14 +139,14 @@
   }
   .slide-enter-active {
     animation: slide-in 1s ease-out forwards;
-    transition: opacity 0.5s;
+    transition: opacity 1s;
   }
   .slide-leave {
 
   }
   .slide-leave-active {
     animation: slide-out 1s ease-out forwards;
-    transition: opacity 3s;
+    transition: opacity 1s;
     opacity: 0;
   }
 
