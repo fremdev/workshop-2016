4edee0c4510682840f535402a427aebf3aa42c9e
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-06 13:20:11 +0200

    Add ex20 mixins and filters example

---
+++ vuejs-examples/playground/ex20-mixins/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters and Mixins</h1>
+                <p>{{ text | toUppercase | toLowercase }}</p>
+                <hr>
+                <input v-model="filterText">
+                <ul>
+                  <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+                </ul>
+                <hr>
+                <app-list></app-list>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import { fruitMixin } from './fruitMixin';
+  import List from './List.vue';
+    export default {
+      mixins: [fruitMixin],
+      data() {
+        return {
+          text: 'Hello there'
+        }
+      },
+      filters: {
+        toUppercase(value) {
+          return value.toUpperCase();
+        }
+      },
+      components: {
+        appList: List
+      }  
+    }
+</script>
+
+<style>
+
+</style>
---
+++ vuejs-examples/playground/ex20-mixins/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.filter('toLowercase', value => value.toLowerCase());
+
+// Global mixin example
+Vue.mixin({
+  created() {
+    console.log('Global mixin - created hook');
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
