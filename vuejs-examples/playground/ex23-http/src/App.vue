ecde663feb727c0f11fe5a6da983b26fdc306e33
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-07 16:43:05 +0200

    Add ex23-http - using vue-resource for http connection

---
+++ vuejs-examples/playground/ex23-http/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Http</h1>
+                <div class="form-group">
+                  <label>Username:</label>
+                  <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                  <label>Mail:</label>
+                  <input type="text" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          user: {
+            username: '',
+            email: ''
+          },
+          users: []
+        };
+      },
+      methods: {
+        submit() {
+          this.$http.post('https://catch-of-the-day-6753f.firebaseio.com/data.json', this.user)
+            .then(response => {
+              console.log(response);
+            }, error => {
+              console.log(error);
+            });
+        },
+        fetchData() {
+          this.$http.get('https://catch-of-the-day-6753f.firebaseio.com/data.json')
+            .then(response => {
+              return response.json();
+            })
+            .then(data => {
+              const resArray = [];
+              for (let key in data) {
+                resArray.push(data[key]);
+              }
+              this.users = resArray;
+            });
+        }
+      }
+    }
+</script>
+
+<style>
+</style>
