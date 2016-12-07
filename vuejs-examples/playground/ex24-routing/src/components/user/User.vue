20d1c6939fcd544631a05c05fef71415047f587f
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-07 21:47:01 +0200

    Use route parameters

--- vuejs-examples/playground/ex24-routing/src/components/user/User.vue
+++ vuejs-examples/playground/ex24-routing/src/components/user/User.vue
@@ -1,4 +1,29 @@
 <template>
+  <div>
     <h1>The User Page</h1>
+    <hr>
+    <p>Loaded ID: {{ id }}</p>
+    <button @click="navigatToHome" class="btn btn-primary">To Home</button>
+  </div>
 
 </template>
+
+<script>
+    export default {
+      data() {
+        return {
+          id: this.$route.params.id
+        }
+      },
+      watch: {
+        '$route'(to, from) {
+          this.id = to.params.id;
+        }
+      },
+      methods: {
+        navigatToHome() {
+          this.$router.push('/');
+        }
+      }
+    }
+</script>
