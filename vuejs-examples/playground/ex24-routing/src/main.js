2533f0bf46136c8a2868bdc285da192bdfdeecd9
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-08 17:16:11 +0200

    Use hash fragment and scroll behavior

--- vuejs-examples/playground/ex24-routing/src/main.js
+++ vuejs-examples/playground/ex24-routing/src/main.js
@@ -5,7 +5,17 @@ import { routes } from './routes';
 
 Vue.use(VueRouter);
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return {x: 0, y: 0};
+  }
 });
 
 new Vue({
