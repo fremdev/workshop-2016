e210a5183ef826425654753f042b2332af15e8ff
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-09 12:51:55 +0200

    Add actions and dispatch them in components

--- vuejs-examples/playground/ex25-vuex/src/components/AnotherCounter.vue
+++ vuejs-examples/playground/ex25-vuex/src/components/AnotherCounter.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="increment">Increment</button>
-        <button class="btn btn-primary" @click="decrement">Decrement</button>
+        <button class="btn btn-primary" @click="increment(10)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(10)">Decrement</button>
     </div>
 </template>
 
 <script>
-  import { mapMutations } from 'vuex';
+  import { mapActions } from 'vuex';
 
   export default {
       methods: {
-        ...mapMutations([
+        ...mapActions([
           'increment',
           'decrement'
         ])
