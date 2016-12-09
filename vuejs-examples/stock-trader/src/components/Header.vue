5d64b8bb3acf5b61776b7458bf35a1784b5370fa
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-09 20:50:29 +0200

    Add stock-trader app boilerplate with basic routing

---
+++ vuejs-examples/stock-trader/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>The Header Component</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ vuejs-examples/stock-trader/src/components/stocks/Stocks.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>The Stocks Component</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
