54f8650c339695c2da76c7ac9a9bb1fda6d9c440
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-11 22:57:00 +0200

    Add BeepList and Beep components

--- vuejs-examples/beeper/beeper-client/src/components/dash/Newsfeed.vue
+++ vuejs-examples/beeper/beeper-client/src/components/dash/Newsfeed.vue
@@ -1,10 +1,17 @@
 <template>
-  
+  <div id="newsfeed">
+    <h2 class="m-t-0 m-b-20">Latest Beeps</h2>
+    <beep-list></beep-list>
+  </div>
 </template>
 
 <script>
+import BeepList from 'components/dash/BeepList.vue';
 export default {
-  name: 'newsfeed'
+  name: 'newsfeed',
+  components: {
+    beepList: BeepList
+  }
 }
 </script>
 
