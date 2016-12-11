6dc2db3d02a5e5053f1f6b304a1332dc69d84e5c
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-11 23:10:07 +0200

    Format date and time with moment.js

--- vuejs-examples/beeper/beeper-client/src/components/dash/Beep.vue
+++ vuejs-examples/beeper/beeper-client/src/components/dash/Beep.vue
@@ -23,7 +23,10 @@
                 <i class="fa fa-heart" :class="[beep.liked ? 'fa-heart' : 'fa-heart-o']"></i>
                 {{beep.likes}} like{{beep.likes != 1 ? 's' : ''}}
             </button>
-            <strong class="pull-right"><i class="fa fa-calendar"></i> {{beep.created_at}} <i class="fa fa-clock-o"></i> {{beep.created_at}}</strong>
+            <strong class="pull-right">
+              <i class="fa fa-calendar"></i> {{beepDate(beep.created_at)}}
+              <i class="fa fa-clock-o"></i> {{beepTime(beep.created_at)}}
+            </strong>
         </div>
     </div>
 </template>
@@ -31,12 +34,19 @@
 
 
 <script>
+  import moment from 'moment';
     export default {
         name: 'beep',
         props: {beep:{}},
         methods: {
             likeBeep: function () {
 
+            },
+            beepDate(timestamp) {
+              return moment(timestamp * 1000).format("DD-MM-YYYY");
+            },
+            beepTime(timestamp) {
+              return moment(timestamp * 1000).format("HH:mm");
             }
         }
     }
